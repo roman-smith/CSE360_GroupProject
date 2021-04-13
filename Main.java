1281a12c8ec46c4cdf7ee3405d29b1c0ef69813c
roman-smith <75827047+roman-smith@users.noreply.github.com>
2021-04-13 13:22:18 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.lang.*;
+import java.time.*;
+import java.util.*;
+import java.io.*;
+
+// idk if we need these in this file or if we need to have a separate JavaFX application file
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class Main {
+	// main method
+	public static void main(String[] args) {
+		// main list of Patient objects to store all of our data
+		ArrayList<Patient> patientList = new ArrayList<Patient>();
+	}
+
+	// other methods
+	// IDK WHAT RETURN TYPE EACH HAS, WE CAN CHANGE IF NEED BE
+	public void loadData(ArrayList<Patient> patientList) {
+
+	}
+
+	public void addData(ArrayList<Patient> patientList) {
+
+	}
+
+	public void saveData(ArrayList<Patient> patientList) {
+
+	}
+
+	public void visualizeData(ArrayList<Patient> patientList) {
+
+	}
+}
